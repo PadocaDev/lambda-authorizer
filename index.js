9adec4c39d2440ebf21756fb5d47080e6088352c
Murillo Godoi <murillo.godoi@alura.com.br>
2023-11-06 22:29:33 -0300

    cria lambda e adiciona script de deploy

---
+++ index.js
@@ -0,0 +1,56 @@
+const jwt = require("jsonwebtoken");
+
+function generatePolicyDocument(effect, methodArn) {
+    if (!effect || !methodArn) return null;
+  
+    const policyDocument = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: methodArn
+        }
+      ]
+    };
+  
+    return policyDocument;
+  }
+
+function generateAuthResponse(principalId, effect, methodArn) {
+  const policyDocument = generatePolicyDocument(effect, methodArn);
+
+  return {
+    principalId,
+    policyDocument
+  };
+}
+
+function extractTokenFromHeader(event) {
+  if (event.authorizationToken && event.authorizationToken.split(' ')[0] === 'Bearer') {
+    return event.authorizationToken.split(' ')[1];
+  } else {
+    return event.authorizationToken;
+  }
+}
+
+exports.handler = (event, context, callback) => {
+  const token = extractTokenFromHeader(event) || '';
+  const methodArn = event.methodArn;
+
+  if(!token) return callback(null, generateAuthResponse(null, 'Allow', methodArn)); //permite seguir para usuários que não querem se identificar
+
+  const secret = process.env.JWT_SECRET;
+
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    if (decoded && decoded.cpf) {
+      return callback(null, generateAuthResponse(decoded.cpf, 'Allow', methodArn));
+    } else {
+      return callback(null, generateAuthResponse('user', 'Deny', methodArn));
+    }
+  } catch (e) {
+    return callback(null, 'Unauthorized');
+  }
+}
